ba5b2a819317dd192d46c22028b2b90bd15eb2cc
moshe malka <moshemalka2014@gmail.com>
2017-10-04 01:33:49 +0300

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,139 @@
+﻿def constructSubmatrix(matrix, rowsToDelete, columnsToDelete):
+    _matrix = matrix
+    for r in rowsToDelete:
+        del _matrix[r]
+        r-=1
+    for i in range(len(columnsToDelete)):
+        c=columnsToDelete[i]
+        if(i is not 0):
+            c-=1
+        for r in _matrix:
+            r.pop(c)
+        
+    return _matrix
+
+
+
+
+'''
+
+Given a matrix, find its submatrix obtained by deleting the specified rows and columns.
+
+Example
+
+For
+
+matrix = [[1, 0, 0, 2], 
+          [0, 5, 0, 1], 
+          [0, 0, 3, 5]]
+rowsToDelete = [1] and columnsToDelete = [0, 2], the output should be
+
+constructSubmatrix(matrix, rowsToDelete, columnsToDelete) = [[0, 2],
+                                                             [0, 5]]
+Input/Output
+
+[time limit] 4000ms (py)
+[input] array.array.integer matrix
+
+2-dimensional array of integers.
+
+Guaranteed constraints:
+1 ≤ matrix.length ≤ 5,
+1 ≤ matrix[0].length ≤ 5,
+0 ≤ matrix[i][j] ≤ 15.
+
+[input] array.integer rowsToDelete
+
+Array of indices (0-based) of rows to be deleted.
+
+Guaranteed constraints:
+0 ≤ rowsToDelete.length ≤ 2,
+0 ≤ rowsToDelete[i] < matrix.length.
+
+[input] array.integer columnsToDelete
+
+Array of indices (0-based) of columns to be deleted.
+
+Guaranteed constraints:
+0 ≤ columnsToDelete.length ≤ 2,
+0 ≤ columnsToDelete[i] < matrix[0].length.
+
+[output] array.array.integer
+
+GIVE UP
+CODE.PY
+Saved
+Python2
+
+def constructSubmatrix(matrix, rowsToDelete, columnsToDelete):
+    _matrix = matrix
+    for r in rowsToDelete:
+        del _matrix[r]
+        r-=1
+    for i in range(len(columnsToDelete)):
+        c=columnsToDelete[i]
+        if(i is not 0):
+            c-=1
+        for r in _matrix:
+            r.pop(c)
+        
+    return _matrix
+1
+def constructSubmatrix(matrix, rowsToDelete, columnsToDelete):
+2
+    _matrix = matrix
+3
+    for r in rowsToDelete:
+4
+        del _matrix[r]
+5
+        r-=1
+6
+    for i in range(len(columnsToDelete)):
+7
+        c=columnsToDelete[i]
+8
+        if(i is not 0):
+9
+            c-=1
+10
+        for r in _matrix:
+11
+            r.pop(c)
+12
+        
+13
+    return _matrix
+TESTS
+CUSTOM TESTS
+Sample tests passed
+Click Submit to run the full test set and to save your solution
+Test 1
+Test 2
+Test 3
+Test 4
+Test 5
+Formatting
+Font Size
+14px
+Code Completion
+Auto-brackets
+Editor
+Type
+Sublime
+Theme
+Dark
+Hotkeys
+CTRL + Enter
+Submit Solution
+CTRL + R
+Run Solution
+CTRL + S
+Save Source Code
+SKIP
+RUN TESTS
+SUBMIT
+
+
+
+''''
---
+++ assignment2.py
@@ -0,0 +1,72 @@
+﻿def periodicSequence(s0, a, b, m):
+    s=[]
+    s.append(s0)
+    for i in range(1,100):
+        s.append((a * s[i - 1] + b) % m)
+    
+    count=0
+    d = {i:s.count(i) for i in s}
+    # for i in d:
+    #     if d[i]==max(d.values()): count+=1
+            
+    # this works
+    for j in d:
+        if d[j]>1: count+=1
+    return count
+
+
+
+
+
+
+'''
+
+A periodic sequence s is defined as follows:
+
+s[0], a, b and m are all given positive integers;
+s[i] for i > 0 is equal to (a * s[i - 1] + b) mod m.
+Find the period of s, i.e. the smallest integer T such that for each i > k (for some integer k): s[i] = s[i + T].
+
+Example
+
+For s0 = 11, a = 2, b = 6 and m = 12, the output should be
+periodicSequence(s0, a, b, m) = 2.
+
+The sequence would look like this: 11, 4, 2, 10, 2, 10, 2, 10, 2, 10....
+
+For s0 = 1, a = 2, b = 3 and m = 5, the output should be
+periodicSequence(s0, a, b, m) = 4.
+
+The sequence would look like this: 1, 0, 3, 4, 1, 0, 3, 4, 1, 0, 3, 4....
+
+Input/Output
+
+[time limit] 4000ms (py)
+[input] integer s0
+
+a positive integer representing s[0].
+
+Guaranteed constraints:
+1 ≤ s0 ≤ 100,
+s0 < m.
+
+[input] integer a
+
+Guaranteed constraints:
+2 ≤ a ≤ 100.
+
+[input] integer b
+
+Guaranteed constraints:
+2 ≤ b ≤ 100.
+
+[input] integer m
+
+Guaranteed constraints:
+5 ≤ m ≤ 100.
+
+[output] integer
+
+The sequence period.
+
+'''
