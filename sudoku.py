ada9275713b0e0001f0d8c3ea7f46c37c52d6e8e
moshe malka <moshemalka2014@gmail.com>
2017-10-05 00:40:50 +0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -2,19 +2,26 @@
 def sudoku2(grid):
     if len(grid)!= 9: return False
     for row in grid:
-        if hasDuplicate(row) or len(row)!=9:
+        if hasDuplicateInRow(row) or len(row)!=9:
             return False
     column_checked=[]
     for i in range(len(grid)):
         for j in range(len(grid)):
             column_checked.append(grid[j][i])
-        if hasDuplicate(column_checked):
+        if hasDuplicateInRow(column_checked):
             return False
         else:
             column_checked=[]
+    cubes_indexes = [[(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)], [(0, 3), (0, 4), (0, 5), (1, 3), (1, 4), (1, 5), (2, 3), (2, 4), (2, 5)], [(0, 6), (0, 7), (0, 8), (1, 6), (1, 7), (1, 8), (2, 6), (2, 7), (2, 8)], [(3, 0), (3, 1), (3, 2), (4, 0), (4, 1), (4, 2), (5, 0), (5, 1), (5, 2)], [(3, 3), (3, 4), (3, 5), (4, 3), (4, 4), (4, 5), (5, 3), (5, 4), (5, 5)], [(3, 6), (3, 7), (3, 8), (4, 6), (4, 7), (4, 8), (5, 6), (5, 7), (5, 8)], [(6, 0), (6, 1), (6, 2), (7, 0), (7, 1), (7, 2), (8, 0), (8, 1), (8, 2)], [(6, 3), (6, 4), (6, 5), (7, 3), (7, 4), (7, 5), (8, 3), (8, 4), (8, 5)], [(6, 6), (6, 7), (6, 8), (7, 6), (7, 7), (7, 8), (8, 6), (8, 7), (8, 8)]]
+    for cube in cubes_indexes:
+        temp_cube = []
+        for x in cube:
+            temp_cube.append(grid[x[0]][x[1]])
+        if(hasDuplicateInRow(temp_cube)):
+            return False
     return True   
         
-def hasDuplicate(a):
+def hasDuplicateInRow(a):
     import string
     seen=set()
     for x in a:
@@ -24,7 +31,6 @@ def hasDuplicate(a):
             else:
                 return True
     return False
-
 
 
 
